fd5796eeea12c49089da4acdb1d5e3230790a4d3
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-28 15:35:30 +0100

    changed test folder

--- installApkAndRunTest.sh
+++ installApkAndRunTest.sh
@@ -1,4 +1,4 @@
 ./gradlew installAlphaDebug
 sleep 1
 adb shell am start -n "it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER >> result.txt
-./gradlew test --info
+./gradlew connectedAndroidTest --info
