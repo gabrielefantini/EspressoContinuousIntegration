5d7822d0d379e6dc31b5788c9a323fe8fc06f801
gabrielefantini <gabrielefantini97@gmail.com>
2022-01-13 16:09:44 +0100

    show devices available and save test results

--- installApkAndRunTest.sh
+++ installApkAndRunTest.sh
@@ -1,5 +1,4 @@
 ./gradlew installAlphaDebug
 sleep 1
-advmanager list device
 adb shell am start -n it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity
 ./gradlew connectedAndroidTest --info
