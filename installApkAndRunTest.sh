d10a7e9399da08bf6d6d12643d89d78c6c6a1b28
gabrielefantini <gabrielefantini97@gmail.com>
2022-02-22 12:53:51 +0100

    trying test isolation

--- installApkAndRunTest.sh
+++ installApkAndRunTest.sh
@@ -1,4 +1,4 @@
 ./gradlew installAlphaDebug
 sleep 1
 adb shell am start -n it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity
-./gradlew connectedAndroidTest --info
+./gradlew connectedCheck --info
