0ae1ad8d1a89701f29ff083ebaff85d0e2c19d8d
gabrielefantini <gabrielefantini97@gmail.com>
2022-02-26 18:06:09 +0100

    added tests

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/testForThesis/MyTestCases.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/testForThesis/MyTestCases.java
@@ -5,26 +5,39 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
 import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withChild;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.PerformException;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.espresso.util.HumanReadables;
 import androidx.test.espresso.util.TreeIterables;
@@ -42,6 +55,7 @@ import org.junit.runners.MethodSorters;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import it.feio.android.checklistview.widgets.EditTextMultiLineNoEnter;
 import it.feio.android.omninotes.MainActivity;
 import it.feio.android.omninotes.R;
 
@@ -79,19 +93,135 @@ public class MyTestCases {
     }
 
     @Test
-    public void insertNewNoteB() throws IOException, InterruptedException {
+    public void insertNewCheckList() throws IOException, InterruptedException {
         //  SETUP
         ActivityScenario activityScenario =
                 ActivityScenario.launch(MainActivity.class);
 
-        onView(withText("New Note")).check(doesNotExist());
-        onView(withText("New Note Content")).check(doesNotExist());
+        onView(withId(R.id.fab_expand_menu_button))
+                .perform(click());
+        onView(withId(R.id.fab_checklist))
+                .perform(click());
+        //Check if navigation on another fragment has happened
+        onView(withId(R.id.detail_root))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.detail_tile_card))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.detail_content_card))
+                .check(matches(isDisplayed()));
+        //Inserting new title
+        onView(withId(R.id.detail_title))
+                .perform(typeText("New Checklist"));
+        onView(withClassName(containsString("EditTextMultiLineNoEnter")))
+                .perform(typeText("New Checklist Item"));
+        pressBack();
+        onView(withText("New Checklist"))
+                .check(matches(isDisplayed()));
+        onView(withText("New Checklist Item"))
+                .check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void archiveNote() throws IOException, InterruptedException {
+        //  SETUP
+        ActivityScenario activityScenario =
+                ActivityScenario.launch(MainActivity.class);
+
+        insertNote("New Note", "New Note Content");
+
+        //Swipe left
+        onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeLeft()));
+        //Open drawer two times to avoid animation of swipe left
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isOpen(Gravity.LEFT)))
+                .perform(DrawerActions.close());
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        //Click on Archive on the drawer menu
+        onView(withText("Archive")).check(matches(isDisplayed()));
+        onView(withText("Archive")).perform(click());
+        //Check if actually the note has been archived
+        onView(withText("New Note")).check(matches(isDisplayed()));
+        onView(withText("New Note Content")).check(matches(isDisplayed()));
+
+
+    }
+
+    @Test
+    public void searchNote() throws IOException, InterruptedException {
+        //  SETUP
+        ActivityScenario activityScenario =
+                ActivityScenario.launch(MainActivity.class);
+
+        insertNote("X", "");
+        insertNote("Y", "");
+        insertNote("Z", "");
+
+        onView(withId(R.id.menu_search)).perform(click());
+        onView(withId(R.id.search_src_text))
+                .perform(typeText("X"))
+                .perform(pressKey(KeyEvent.KEYCODE_ENTER));
+
+        onView(allOf(withText("X"),withId(R.id.note_title))).check(matches(isDisplayed()));
+        onView(allOf(withText("Y"),withId(R.id.note_title))).check(doesNotExist());
+        onView(allOf(withText("Z"),withId(R.id.note_title))).check(doesNotExist());
+
+
+    }
+
+    @Test
+    public void insertNoteWithReminder() throws IOException, InterruptedException {
+        //  SETUP
+        ActivityScenario activityScenario =
+                ActivityScenario.launch(MainActivity.class);
+
+        onView(withId(R.id.fab_expand_menu_button))
+                .perform(click());
+        onView(withId(R.id.fab_note))
+                .perform(click());
+        //Check if navigation on another fragment has happened
+        onView(withId(R.id.detail_root))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.detail_tile_card))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.detail_content_card))
+                .check(matches(isDisplayed()));
+        //Inserting new title
+        onView(withId(R.id.detail_title))
+                .perform(typeText("New Note With Reminder"));
+        onView(withId(R.id.reminder_icon))
+                .perform(click());
+        onView(allOf(withText("OK"), withParent(withId(R.id.button_layout))))
+                .check(matches(isDisplayed()))
+                .perform(click());
+        pressBack();
+        onView(withId(R.id.alarmIcon))
+                .check(matches(isDisplayed()));
 
     }
     /*=====================================
      * Utils functions
      * =====================================*/
+    private void insertNote(String title, String body){
+        //Click on fab button
+        onView(withId(R.id.fab_expand_menu_button)).perform(click());
+        onView(withId(R.id.fab_note)).perform(click());
+        //Check if navigation on another fragment has happened
+        onView(withId(R.id.detail_root)).check(matches(isDisplayed()));
+        onView(withId(R.id.detail_tile_card)).check(matches(isDisplayed()));
+        onView(withId(R.id.detail_content_card)).check(matches(isDisplayed()));
+        //Inserting new title
+        onView(withId(R.id.detail_title)).perform(typeText(title));
+        onView(withId(R.id.detail_content)).perform(typeText(body));
 
+        pressBack();
+        pressBack();
+    }
     //waiting for a textView to appear
     private static ViewAction waitForText(
             final String viewText,
